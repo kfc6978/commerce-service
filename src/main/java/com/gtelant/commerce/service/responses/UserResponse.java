55ead74206d246f1d165257b3081d692831b56d2
Judy, Chiang <xji6xo6n@gmail.com>
2025-09-17 17:00:06 +0800

    add function of User

--- src/main/java/com/gtelant/commerce/service/responses/UserResponse.java
+++ src/main/java/com/gtelant/commerce/service/responses/UserResponse.java
@@ -1,6 +1,7 @@
 package com.gtelant.commerce.service.responses;
 
 
+import com.gtelant.commerce.service.models.User;
 import com.gtelant.commerce.service.models.UserSegment;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,4 +23,13 @@ public class UserResponse {
     private boolean hasNewsletter;
     private List<UserSegment> userSegments;
 
+    public UserResponse (User user){
+        this.id = user.getId();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.birthday = user.getBirthday();
+        this.lastSeenAt = user.getLastSeenAt();
+        this.hasNewsletter = user.isHasNewsletter();
+        this.userSegments = user.getUserSegments();
+    }
 }
